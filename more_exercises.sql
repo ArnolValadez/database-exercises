d7c031dd3e6f9c53494deceb780764ba12674a5c
ArnolValadez <a7xfav@gmail.com>
2021-07-05 21:03:59 -0500

    add more exercises

---
+++ more_exercises.sql
@@ -0,0 +1,3 @@
+# Employees Database
+
+1. How much do the current managers of each department get paid, relative to the average salary for the department? Is there any department where the department manager gets paid less than the average salary?
